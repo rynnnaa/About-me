9e207ded613fcf80378d01f0b7d891d5c3a4c94e
xgingerbreadmanx <rjjohnson1204@gmail.com>
2018-10-25 13:37:46 -0700

    further updates 10-25

--- script.js
+++ script.js
@@ -3,101 +3,98 @@
 console.log('userName', userName);
 
 var userName = prompt('Welcome to the site. What is your name?');
-    alert(' Nice to meet you ' + userName + '.'); 
-     console.log('userName', userName);
+alert(' Nice to meet you ' + userName + '.');
+console.log('userName', userName);
 
 alert('please respond to the following five questions with yes or no');
 
 console.log('candy', candy);
-var candy  = prompt('Do I like candy?');
+var candy = prompt('Do I like candy?');
 var res = candy.toLowerCase()
 console.log('candy', candy);
-if (candy === 'yes')
-{
-alert(' Nope!');
-}       
+if (candy === 'yes') {
+  alert(' Nope!');
+}
 else if (candy === 'no') {
-alert(' Correct!');
+  alert(' Correct!');
 
-}   
+}
 
 console.log('color', color);
-var color  = prompt('Is my favorite color blue?');
+var color = prompt('Is my favorite color blue?');
 var res = color.toLowerCase()
 console.log('color', color);
-if (color === 'yes')
-{
-alert(' Sure is!');
-}       
+if (color === 'yes') {
+  alert(' Sure is!');
+}
 else if (candy === 'no') {
-alert(' Wrong!');
+  alert(' Wrong!');
 }
 console.log('height', height);
-var height  = prompt('Am I six feet tall?');
+var height = prompt('Am I six feet tall?');
 var res = height.toLowerCase()
 console.log('height', height);
-if (height === 'yes')
-{
-alert(' Nope, almost six foot.');
-}       
+if (height === 'yes') {
+  alert(' Nope, almost six foot.');
+}
 else if (height === 'no') {
-alert(' I didnt make the six foot club. ')
+  alert(' I didnt make the six foot club. ')
 }
 
 
 console.log('pie', pie);
-var pie  = prompt(' Do I like apple pie?');
+var pie = prompt(' Do I like apple pie?');
 var res = pie.toLowerCase()
 console.log('pie', pie);
-if (pie === 'yes')
-{
-alert(' Sure is!');
-}       
+if (pie === 'yes') {
+  alert(' Sure is!');
+}
 else if (height === 'no') {
-alert(' Wrong ')
+  alert(' Wrong ')
 }
 
 console.log('hand', hand);
-var hand  = prompt(' Am I left handed? ');
+var hand = prompt(' Am I left handed? ');
 var res = hand.toLowerCase()
 console.log('hand', hand);
-if (hand === 'yes')
-{
-alert(' I am not left handed.');
-}       
+if (hand === 'yes') {
+  alert(' I am not left handed.');
+}
 else if (height === 'no') {
-alert(' Correct ')
+  alert(' Correct ')
 }
 
 
 var favoriteNumber = 3
 var counter = 0
 console.log('numberGuess', numberGuess);
-var numberGuess  = parseInt(prompt(' What is my favorite number? You have four guesses '));
+var numberGuess = parseInt(prompt(' What is my favorite number? You have four guesses '));
 console.log('numberGuess', numberGuess);
 
- while( numberGuess !== favoriteNumber && counter < 3){
-     counter++; 
- 
-if (numberGuess === 3){
-    alert( 'Correct')
-}
-if ( numberGuess > favoriteNumber){
-    parseInt(alert( ' Close, a little lower!'));
-}
-else if ( numberGuess < 3){
-    parseInt(alert( ' Close, a little higher!'));
-    }
-// else(alert( 'Correct'));
-//  if (numberGuess === 3){
-//     alert( 'Correct')
- }
+while (numberGuess !== favoriteNumber && counter < 3) {
+  counter++;
 
+  // if (numberGuess === 3) {
+  //   alert('Correct')
+  }
+  if (numberGuess > favoriteNumber) {
+    parseInt(alert(' Close, a little lower!'));
+  }
+  else if (numberGuess < 3) {
+    parseInt(alert(' Close, a little higher!'));
+  // } else{
+  //   numberGuess = parseInt(prompt( 'correct'));
+  }
+  // else(alert( 'Correct'));
+  //  if (numberGuess === 3){
+  //     alert( 'Correct')
+
+// numberGuess = parseInt(prompt(numberGuess + ' is not a number'));
 
     // else(numberGuess){
     //     alert( 'Correct')
 // }
-    
+
 
 
 
